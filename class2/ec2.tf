e651d31a4b74f862bb4bd01d784234910c69eeb0
eliza <elizadevops@gmail.comgit config --global user.email elizadevops@gmail.com>
2025-07-10 01:56:49 +0000

    added class2 files

---
+++ class2/ec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "web" {
+  ami           = "ami-05ffe3c48a9991133"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-04f2fd78749f0597a"
+  key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output ec2 {
+    value = aws_instance.web.public_ip
+}
