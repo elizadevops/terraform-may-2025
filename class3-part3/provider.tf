587b248fad52c9477adaa94e12b5684580e1ff4a
eliza <elizadevops@gmail.comgit config --global user.email elizadevops@gmail.com>
2025-07-15 02:10:53 +0000

    class3 files

---
+++ class3-part3/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "world-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
